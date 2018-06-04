6540a811eef79c75dd7b4214c36f0b04415c2341
FilinRN <rfilin@rambler.ru>
2018-06-04 16:57:45 +0600

    Изменения в MainActivity

--- app/src/main/java/personalarea/ru/personalarea/MainActivity.java
+++ app/src/main/java/personalarea/ru/personalarea/MainActivity.java
@@ -1,10 +1,21 @@
 package personalarea.ru.personalarea;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.Gravity;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -16,26 +27,60 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        //layotVertButton = (LinearLayout) findViewById(R.id.LayotVertButton);
-        TableLayout tablelayout = new TableLayout(this);
-        tablelayout.setStretchAllColumns(true);
-        tablelayout.setShrinkAllColumns(true);
+
+        TableLayout table = new TableLayout(this);
+        table.setStretchAllColumns(true);
+        table.setShrinkAllColumns(true);
+        table.setBackgroundResource(R.drawable.d_back_para_count);
         TableRow rowTitle = new TableRow(this);
         rowTitle.setGravity(Gravity.CENTER_HORIZONTAL);
-        TableRow rowDayLabels = new TableRow(this);
-        TableRow rowHighs = new TableRow(this);
-        TableRow rowLows = new TableRow(this);
-        TableRow rowConditions = new TableRow(this);
-        rowConditions.setGravity(Gravity.CENTER);
-        setContentView(tablelayout);
-     /*   ImageButton btnNew = new ImageButton(this);
-        //btnNew.setText("ButtonTest");
-        btnNew.setImageResource(R.drawable.doodlejump);
-        layotVertButton.addView(btnNew);
-        Button btnNew1 = new Button(this);
-        btnNew1.setText("ButtonTest");
-        layotVertButton.addView(btnNew1);
-        */
+        rowTitle.setPadding(0, 10, 0, 0);
+
+        TextView title = new TextView(this);
+        title.setText("Общая информация");
+        title.setBackgroundResource(R.drawable.d_back_compound_maroon);
+        title.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 25);
+        title.setGravity(Gravity.CENTER);
+        title.setTypeface(Typeface.SANS_SERIF, Typeface.BOLD);
+
+        TableRow.LayoutParams params = new TableRow.LayoutParams();
+        params.span = 2;
+        rowTitle.addView(title, params);
+
+        //TableRow emptyRow = new TableRow(this);
+        //TextView emptyView = new TextView(this);
+        //emptyRow.addView(emptyView);
+
+        TableRow dataLS = new TableRow(this);
+        //dataLS.setGravity(Gravity.LEFT);
+        dataLS.setGravity(Gravity.CENTER);
+        dataLS.setPadding(1, 10, 1, 1);
+
+        //Button btnNew = new Button(this);
+        ImageButton btnNew = new ImageButton(this);
+        //btnNew.setImageResource(R.drawable.d_back_compound_green);
+        btnNew.setBackgroundResource(R.drawable.d_back_compound_green);
+        TextView textBtn = new TextView(this);
+        textBtn.setText("Подробно");
+        textBtn.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 17);
+        textBtn.setGravity(Gravity.CENTER);
+
+        FrameLayout fl = new FrameLayout(this);
+        fl.addView(btnNew);
+        fl.addView(textBtn);
+
+        TextView addr = new TextView(this);
+        addr.setText("Омск, ул. Перелета, д.8, кв. 379");
+        addr.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 17);
+        addr.setGravity(Gravity.CENTER);
+        addr.setBackgroundResource(R.drawable.d_back_compound_green);
+
+        dataLS.addView(fl);
+        dataLS.addView(addr);
+
+        table.addView(rowTitle);
+        //table.addView(emptyRow);
+        table.addView(dataLS);
+        setContentView(table);
     }
 }
